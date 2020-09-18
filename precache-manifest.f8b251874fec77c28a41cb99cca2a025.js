8e93ea23a2e59e5916a7194d33db496251ba6292
[suraj] <[suraj1996xyz@gmail.com]>
2020-09-18 22:13:39 +0530

    Updates

---
+++ precache-manifest.f8b251874fec77c28a41cb99cca2a025.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c517df1197e321bbd4de0122f117a5",
+    "url": "/Corona/index.html"
+  },
+  {
+    "revision": "803ec2997505b11638d8",
+    "url": "/Corona/static/css/main.2b56d56a.chunk.css"
+  },
+  {
+    "revision": "0e7fbf2961880dc588f2",
+    "url": "/Corona/static/js/2.63e965f0.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/Corona/static/js/2.63e965f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "803ec2997505b11638d8",
+    "url": "/Corona/static/js/main.2d6fd08d.chunk.js"
+  },
+  {
+    "revision": "80f482ad8989c86778de",
+    "url": "/Corona/static/js/runtime-main.dd318a6e.js"
+  },
+  {
+    "revision": "93ebcd12af8d7f948a449a584f03e57c",
+    "url": "/Corona/static/media/coronavirus.93ebcd12.png"
+  }
+]);
